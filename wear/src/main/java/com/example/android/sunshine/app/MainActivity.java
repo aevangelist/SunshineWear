c676bf25f438061433dcf2ea4ac4e34fc6247c3e
aevangelist <evangelista.aleli@gmail.com>
2016-03-26 15:05:54 -0400

    Set up Broadcast receiver

--- wear/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ wear/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.android.sunshine.app;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static final String WEARABLE_DATA_PATH = "/wearable_data";
+    private static final String MIN_TEMP = "min";
+    private static final String MAX_TEMP = "max";
 
     private TextView mTextView;
 
@@ -20,5 +31,36 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        // Register the local broadcast receiver
+        IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
+        MessageReceiver messageReceiver = new MessageReceiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter);
+
+        //Toast.makeText(this, "dhg;ksdhgsdg.", Toast.LENGTH_LONG).show();
+        Log.i("MainActivity", "Running the watch");
     }
+
+    public class MessageReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+
+            String message = intent.getStringExtra(MIN_TEMP);
+            Log.i("MainActivity", "Broadcast received on watch: " + message);
+
+            Bundle data = intent.getBundleExtra(MIN_TEMP);
+
+            // Display received data in UI
+            String display = "Received from the data Layer\n" +
+                    "Hole: " + data.getString("hole") + "\n" +
+                    "Front: " + data.getString("front") + "\n" +
+                    "Middle: "+ data.getString("middle") + "\n" +
+                    "Back: " + data.getString("back");
+            mTextView.setText(display);
+        }
+    }
+
 }
